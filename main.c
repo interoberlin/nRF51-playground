4bc0e709abc4293ed3ee0a152b5b7bbcb1405c60
Matthias <mail@matthiasbock.net>
2015-08-27 13:01:37 +0200

    compiles for the first time

---
+++ main.c
@@ -0,0 +1,34 @@
+
+/**
+* This example shows how to configure GPIO pins as outputs which can also be used to drive LEDs.
+* Each LED is set on one at a time and each state lasts 100 milliseconds.
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+#define LED_PIN 21
+
+void _exit() {};
+
+int main(void)
+{
+    uint8_t output_state = 0;
+    
+    nrf_gpio_pin_dir_set(LED_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
+    
+    while(true)
+    {
+        if (output_state == 1)
+            nrf_gpio_pin_set(LED_PIN);
+        else
+            nrf_gpio_pin_clear(LED_PIN);
+        output_state = (output_state + 1) & 1;
+        nrf_delay_ms(100);
+    }
+      
+    return 0;
+}
