a9c6414a1b5f3c95f67f493f41dc127463847cea
Matthias <mail@matthiasbock.net>
2015-11-10 22:47:58 +0100

    began porting BLESSED (not compiling)

---
+++ radio.c
@@ -0,0 +1,302 @@
+
+/**
+ * Radio frequency unit library for the Nordic Semiconductor nRF51 series
+ *
+ * Authors:
+ *      Paulo B. de Oliveira Filho <pauloborgesfilho@gmail.com>
+ *      Claudio Takahasi <claudio.takahasi@gmail.com>
+ *      João Paulo Rechi Vita <jprvita@gmail.com>
+ *      Matthias Bock <mail@matthiasbock.net>
+ *
+ * License: GNU GPLv3
+ */
+
+#include "radio.h"
+
+#define MAX_BUF_LEN                 RADIO_MAX_PDU
+#define MAX_PAYLOAD_LENGTH            (RADIO_MAX_PDU - 2)
+
+static uint8_t inbuf[MAX_BUF_LEN] __attribute__ ((aligned));
+static uint8_t *outbuf;
+
+static volatile uint8_t status;
+static volatile uint32_t flags;
+
+// states of the internal state machine
+#define STATUS_INITIALIZED          1
+#define STATUS_RX                   2
+#define STATUS_TX                   4
+#define STATUS_BUSY                (STATUS_RX | STATUS_TX)
+
+#define default_shortcuts          (RADIO_SHORTCUT_READY_START | RADIO_SHORTCUT_END_DISABLE)
+
+
+static radio_receive_callback_t receive_callback;
+static radio_send_callback_t send_callback;
+
+uint8_t radio_channel_to_frequency(uint8_t channel)
+{
+    /*
+     * nRF51 Series Reference Manual v2.1, section 16.2.19, page 91
+     * Link Layer specification section 1.4.1, Core 4.1, page 2502
+     *
+     * The nRF51822 is configured using the frequency offset from 2400 MHz:
+     * On air frequency = 2400 MHz + RADIO_FREQUENCY MHz
+     */
+    switch (channel)
+    {
+        // advertising channels(?)
+        case 37:
+            return 2;
+        case 38:
+            return 26;
+        case 39:
+            return 80;
+        // data channels(?)
+        default:
+            if (ch > 39)
+                return -1;
+            if (ch < 11)
+                return 4 + (2 * channel);
+            return 6 + (2 * channel);
+    }
+}
+
+/**
+ * Radio interrupt handler
+ *
+ * Included in nrf51_startup.c
+ */
+void RADIO_Handler()
+{
+    uint8_t old_status;
+    bool active;
+
+    RADIO_EVENTS_END = 0UL;
+
+    active = false;
+    old_status = status;
+    status = STATUS_INITIALIZED;
+
+    if (old_status & STATUS_RX)
+    {
+        if (flags & RADIO_FLAGS_TX_NEXT)
+        {
+            flags &= ~RADIO_FLAGS_TX_NEXT;
+            status |= STATUS_TX;
+            active = true;
+            RADIO_PACKETPTR = (uint32_t) outbuf;
+            RADIO_SHORTS &= ~RADIO_SHORTCUT_DISABLED_TXEN;
+        }
+
+        if (receive_callback)
+            receive_callback(inbuf, RADIO_CRCSTATUS, active);
+    }
+    else if (old_status & STATUS_TX)
+    {
+        if (flags & RADIO_FLAGS_RX_NEXT)
+        {
+            flags &= ~RADIO_FLAGS_RX_NEXT;
+            status |= STATUS_RX;
+            active = true;
+            RADIO_PACKETPTR = (uint32_t) inbuf;
+            RADIO_SHORTS &= ~RADIO_SHORTCUT_DISABLED_RXEN;
+        }
+
+        if (send_callback)
+            send_callback(active);
+    }
+}
+
+int16_t radio_set_callbacks(radio_receive_callback_t rcb, radio_send_callback_t scb)
+{
+    receive_callback = rcb;
+    send_callback = scb;
+
+    return 0;
+}
+
+int16_t radio_prepare(uint8_t channel, uint32_t aa, uint32_t crcinit)
+{
+    if (!(status & STATUS_INITIALIZED))
+        return -ENOREADY;
+
+    if (status & STATUS_BUSY)
+        return -EBUSY;
+
+    int8_t frequency = radio_channel_to_frequency(channel);
+
+    if (freq < 0)
+        return -EINVAL;
+
+    RADIO_DATAWHITEIV = channel & 0x3F;
+    RADIO_FREQUENCY = frequency;
+    RADIO_BASE0 = (aa << 8) & 0xFFFFFF00;
+    RADIO_PREFIX0 = (aa >> 24) & RADIO_PREFIX0_AP0_Msk;
+    RADIO_CRCINIT = crcinit;
+
+    return 0;
+}
+
+int16_t radio_send(const uint8_t *data, uint32_t f)
+{
+    status |= STATUS_TX;
+    flags |= f;
+
+    if (f & RADIO_FLAGS_RX_NEXT)
+        RADIO_SHORTS |= RADIO_SHORTCUT_DISABLED_RXEN;
+
+    RADIO_PACKETPTR = (uint32_t) data;
+    RADIO_TASK_TXEN = 1;
+
+    return 0;
+}
+
+int16_t radio_recv(uint32_t f)
+{
+    status |= STATUS_RX;
+    flags |= f;
+
+    if (f & RADIO_FLAGS_TX_NEXT)
+        RADIO_SHORTS |= RADIO_SHORTCUT_DISABLED_TXEN;
+
+    RADIO_PACKETPTR = (uint32_t) inbuf;
+    RADIO_TASK_RXEN = 1;
+
+    return 0;
+}
+
+int16_t radio_stop(void)
+{
+    if (!(status & STATUS_BUSY))
+        return -ENOREADY;
+
+    flags = 0;
+    RADIO_SHORTS = default_shortcuts;
+
+    RADIO_EVENTS_DISABLED = 0;
+    RADIO_TASKS_DISABLE = 1;
+    while (!RADIO_EVENTS_DISABLED)
+        asm("nop");
+
+    status &= ~STATUS_BUSY;
+
+    return 0;
+}
+
+void radio_set_out_buffer(uint8_t *buf)
+{
+    outbuf = buf;
+}
+
+int16_t radio_init(void)
+{
+    // wait for high frequency clock to get started
+    if (!NRF_CLOCK->EVENTS_HFCLKSTARTED)
+    {
+        NRF_CLOCK->TASKS_HFCLKSTART = 1;
+        while (!NRF_CLOCK->EVENTS_HFCLKSTARTED)
+            asm("nop");
+    }
+
+    /*
+     * nRF51 Series Reference Manual v2.1, section 6.1.1, page 18
+     * PCN-083 rev.1.1
+     *
+     * Factory Information Configuration Registers (FICR)
+     * Apply obligatory factory settings: 
+     * Fine tune BLE deviation parameters.
+     */
+    if ((NRF_FICR->OVERRIDEEN & FICR_OVERRIDEEN_BLE_1MBIT_Msk)
+                    == (FICR_OVERRIDEEN_BLE_1MBIT_Override
+                    << FICR_OVERRIDEEN_BLE_1MBIT_Pos))
+    {
+        RADIO_OVERRIDE[0] = NRF_FICR->BLE_1MBIT[0];
+        RADIO_OVERRIDE[1] = NRF_FICR->BLE_1MBIT[1];
+        RADIO_OVERRIDE[2] = NRF_FICR->BLE_1MBIT[2];
+        RADIO_OVERRIDE[3] = NRF_FICR->BLE_1MBIT[3];
+        RADIO_OVERRIDE[4] = NRF_FICR->BLE_1MBIT[4] | 0x80000000;
+    }
+
+    RADIO_MODE = RADIO_MODE_BLE_1MBIT;
+
+    /* Link Layer specification section 4.1, Core 4.1, page 2524
+     * nRF51 Series Reference Manual v2.1, section 16.2.7, page 92
+     *
+     * Set the inter frame space (T_IFS) to 150 us.
+     */
+    RADIO_TIFS = 150;
+
+    /* nRF51 Series Reference Manual v2.1, section 16.2.9, page 88
+     *
+     * Enable data whitening, set the maximum payload length and set the
+     * access address size (3 + 1 octets).
+     */
+    radio_data_whitening_enable;
+    radio_set_max_payload_length(MAX_PAYLOAD_LENGTH);
+    radio_set_access_address_size(3);
+
+    /* nRF51 Series Reference Manual v2.1, section 16.1.4, page 74
+     * nRF51 Series Reference Manual v2.1, section 16.2.14-15, pages 89-90
+     *
+     * Preset the address to use when receive and transmit packets (logical
+     * address 0, which is assembled by base address BASE0 and prefix byte
+     * PREFIX0.AP0.
+     */
+    RADIO_RXADDRESSES = RADIO_RXADDR1;
+    RADIO_TXADDRESS   = RADIO_TXADDR0;
+
+    /* nRF51 Series Reference Manual v2.1, section 16.1.7, page 76
+     * nRF51 Series Reference Manual v2.1, sections 16.1.16-17, page 90
+     *
+     * Configure the CRC length (3 octets), polynominal and set it to
+     * ignore the access address when calculate the CRC.
+     */
+    RADIO_CRCCNF =
+        (RADIO_CRCCNF_LEN_Three << RADIO_CRCCNF_LEN_Pos) |
+        (RADIO_CRCCNF_SKIP_ADDR_Skip << RADIO_CRCCNF_SKIP_ADDR_Pos);
+    RADIO_CRCPOLY = 0x100065B;
+
+    /*
+     * nRF51 Series Reference Manual v2.1, section 16.1.2, page 74
+     * nRF51 Series Reference Manual v2.1, sections 16.1.8, page 87
+     * Link Layer specification section 2.3, Core 4.1, page 2504
+     * Link Layer specification section 2.4, Core 4.1, page 2511
+     *
+     * Configure the header size. The nRF51822 has 3 fields before the
+     * payload field: S0, LENGTH and S1. These fields can be used to store
+     * the PDU header.
+     */
+    RADIO_PCNF0 = (1UL << RADIO_PCNF0_S0LEN_Pos)
+            | (8UL << RADIO_PCNF0_LFLEN_Pos)
+            | (0UL << RADIO_PCNF0_S1LEN_Pos);
+
+    /*
+     * nRF51 Series Reference Manual v2.1, section 16.1.8, page 76
+     * nRF51 Series Reference Manual v2.1, section 16.1.10-11, pages 78-80
+     * nRF51 Series Reference Manual v2.1, section 16.2.1, page 85
+     *
+     * Enable READY_START short: when the READY event happens, initialize
+     * the START task.
+     *
+     * Enable END_DISABLE short: when the END event happens, initialize the
+     * DISABLE task.
+     */
+    RADIO_SHORTS = default_shortcuts;
+
+    // Disable all radio interrupts
+    RADIO_INTENCLR = ~0;
+    // Trigger radio interrupt when an END event happens
+    RADIO_INTENSET = RADIO_INTENSET_END;
+
+    radio_set_callbacks(NULL, NULL);
+    RADIO_TXPOWER = RADIO_TXPOWER_0DBM;
+    radio_set_out_buffer(NULL);
+
+    RADIO_PACKETPTR = (uint32_t) inbuf;
+    memset(inbuf, 0, sizeof(inbuf));
+
+    status = STATUS_INITIALIZED;
+
+    return 0;
+}
---
+++ radio.h
@@ -0,0 +1,258 @@
+
+/**
+ * Radio frequency unit library for the Nordic Semiconductor nRF51 series
+ *
+ * Authors:
+ *      Paulo B. de Oliveira Filho <pauloborgesfilho@gmail.com>
+ *      Claudio Takahasi <claudio.takahasi@gmail.com>
+ *      João Paulo Rechi Vita <jprvita@gmail.com>
+ *      Matthias Bock <mail@matthiasbock.net>
+ *
+ * License: GNU GPLv3
+ */
+
+#ifndef RADIO_H
+#define RADIO_H
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/*
+#include "core_cmFunc.h"
+#include "core_cmInstr.h"
+#include "core_cm0.h"
+*/
+ 
+/*
+ * Constants
+ * as specified in the nRF51 Series Reference Manual
+ */
+
+#define RADIO_BASE              0x40001000
+
+// Tasks
+#define RADIO_TASK_TXEN         (*(volatile uint32_t*) (RADIO_BASE+0x000))   // Enable radio in TX mode
+#define RADIO_TASK_RXEN         (*(volatile uint32_t*) (RADIO_BASE+0x004))   // Enable radio in RX mode
+#define RADIO_TASK_START        (*(volatile uint32_t*) (RADIO_BASE+0x008))   // Start radio
+#define RADIO_TASK_STOP         (*(volatile uint32_t*) (RADIO_BASE+0x00C))   // Stop radio
+#define RADIO_TASK_DISABLE      (*(volatile uint32_t*) (RADIO_BASE+0x010))   // Disable radio
+#define RADIO_TASK_RSSISTART    (*(volatile uint32_t*) (RADIO_BASE+0x014))   // Start the RSSI and take one single sample of the receive signal strength
+#define RADIO_TASK_RSSISTOP     (*(volatile uint32_t*) (RADIO_BASE+0x018))   // Stop the RSSI measurement
+#define RADIO_TASK_BCSTART      (*(volatile uint32_t*) (RADIO_BASE+0x01C))   // Start the bit counter
+#define RADIO_TASK_BCSTOP       (*(volatile uint32_t*) (RADIO_BASE+0x020))   // Stop the bit counter
+
+// Events
+#define RADIO_EVENT_READY       (*(volatile uint32_t*) (RADIO_BASE+0x100))   // Radio has ramped up and is ready to be started
+#define RADIO_EVENT_ADDRESS     (*(volatile uint32_t*) (RADIO_BASE+0x104))   // Address sent or received
+#define RADIO_EVENT_PAYLOAD     (*(volatile uint32_t*) (RADIO_BASE+0x108))   // Packet payload sent or received
+#define RADIO_EVENT_END         (*(volatile uint32_t*) (RADIO_BASE+0x10C))   // Packet sent or received
+#define RADIO_EVENT_DISABLED    (*(volatile uint32_t*) (RADIO_BASE+0x110))   // Radio has been disabled
+#define RADIO_EVENT_DEVMATCH    (*(volatile uint32_t*) (RADIO_BASE+0x114))   // A device address match occurred on the last received packet
+#define RADIO_EVENT_DEVMISS     (*(volatile uint32_t*) (RADIO_BASE+0x118))   // No device address match occurred on the last received packet
+#define RADIO_EVENT_RSSIEND     (*(volatile uint32_t*) (RADIO_BASE+0x11C))   // Sampling of receive signal strength complete. A new RSSI sample is ready for readout from the RSSISAMPLE register.
+#define RADIO_EVENT_BCMATCH     (*(volatile uint32_t*) (RADIO_BASE+0x128))   // Bit counter reached bit count value specified in the BCC register
+
+// Registers
+#define RADIO_SHORTS            (*(volatile uint32_t*) (RADIO_BASE+0x200))   // Shortcut register
+#define RADIO_INTENSET          (*(volatile uint32_t*) (RADIO_BASE+0x304))   // Enable interrupt
+#define RADIO_INTENCLR          (*(volatile uint32_t*) (RADIO_BASE+0x308))   // Disable interrupt
+#define RADIO_CRCSTATUS         (*(volatile uint32_t*) (RADIO_BASE+0x400))   // CRC status
+#define RADIO_RXMATCH           (*(volatile uint32_t*) (RADIO_BASE+0x408))   // Received address
+#define RADIO_RXCRC             (*(volatile uint32_t*) (RADIO_BASE+0x40C))   // CRC field of previously received packet
+#define RADIO_DAI               (*(volatile uint32_t*) (RADIO_BASE+0x410))   // Device address match index
+#define RADIO_PACKETPTR         (*(volatile uint32_t*) (RADIO_BASE+0x504))   // Packet pointer
+#define RADIO_FREQUENCY         (*(volatile uint32_t*) (RADIO_BASE+0x508))   // Frequency
+#define RADIO_TXPOWER           (*(volatile uint32_t*) (RADIO_BASE+0x50C))   // Output power
+#define RADIO_MODE              (*(volatile uint32_t*) (RADIO_BASE+0x510))   // Data rate and modulation
+#define RADIO_PCNF0             (*(volatile uint32_t*) (RADIO_BASE+0x514))   // Packet configuration register 0
+#define RADIO_PCNF1             (*(volatile uint32_t*) (RADIO_BASE+0x518))   // Packet configuration register 1
+#define RADIO_BASE0             (*(volatile uint32_t*) (RADIO_BASE+0x51C))   // Base address 0
+#define RADIO_BASE1             (*(volatile uint32_t*) (RADIO_BASE+0x520))   // Base address 1
+#define RADIO_PREFIX0           (*(volatile uint32_t*) (RADIO_BASE+0x524))   // Prefixes bytes for logical addresses 0-3
+#define RADIO_PREFIX1           (*(volatile uint32_t*) (RADIO_BASE+0x528))   // Prefixes bytes for logical addresses 4-7
+#define RADIO_TXADDRESS         (*(volatile uint32_t*) (RADIO_BASE+0x52C))   // Transmit address select
+#define RADIO_RXADDRESSES       (*(volatile uint32_t*) (RADIO_BASE+0x530))   // Receive address select
+#define RADIO_CRCCNF            (*(volatile uint32_t*) (RADIO_BASE+0x534))   // CRC configuration
+#define RADIO_CRCPOLY           (*(volatile uint32_t*) (RADIO_BASE+0x538))   // CRC polynomial
+#define RADIO_CRCINIT           (*(volatile uint32_t*) (RADIO_BASE+0x53C))   // CRC initial value
+#define RADIO_TIFS              (*(volatile uint32_t*) (RADIO_BASE+0x540))   // Test features enable register
+#define RADIO_TEST              (*(volatile uint32_t*) (RADIO_BASE+0x544))   // Inter Frame Spacing: Time interval in us between consecutive packets
+#define RADIO_RSSISAMPLE        (*(volatile uint32_t*) (RADIO_BASE+0x548))   // RSSI sample
+#define RADIO_STATE             (*(volatile uint32_t*) (RADIO_BASE+0x550))   // Current radio state
+#define RADIO_DATAWHITEIV       (*(volatile uint32_t*) (RADIO_BASE+0x554))   // Data whitening initial value
+#define RADIO_BCC               (*(volatile uint32_t*) (RADIO_BASE+0x560))   // Bit counter compare
+
+/*
+ * Arrays are always pointers in C, therefore no asterisk after uint32_t[x].
+ * Accessing array elements by index (e.g. RADIO_DAB[5]) equals pointer dereferencing in C, therefore no asterisk before (volatile...)
+ * => RADIO_DAB is a pointer to an array of 32bit unsigned integers, which has 8 elements and is located at memory address RADIO_BASE+0x600.
+ */
+#define RADIO_DAB               ((volatile uint32_t[8]) (RADIO_BASE+0x600))  // Device address base
+
+/*
+#define RADIO_DAB_0             (*(volatile uint32_t*) (RADIO_BASE+0x600))   // 
+#define RADIO_DAB_1             (*(volatile uint32_t*) (RADIO_BASE+0x604))   // 
+#define RADIO_DAB_2             (*(volatile uint32_t*) (RADIO_BASE+0x608))   // 
+#define RADIO_DAB_3             (*(volatile uint32_t*) (RADIO_BASE+0x60C))   // 
+#define RADIO_DAB_4             (*(volatile uint32_t*) (RADIO_BASE+0x610))   // 
+#define RADIO_DAB_5             (*(volatile uint32_t*) (RADIO_BASE+0x614))   // 
+#define RADIO_DAB_6             (*(volatile uint32_t*) (RADIO_BASE+0x618))   // 
+#define RADIO_DAB_7             (*(volatile uint32_t*) (RADIO_BASE+0x61C)    //
+*/
+
+#define RADIO_DAP               ((volatile uint32_t[8]) (RADIO_BASE+0x620))  // Device address prefix
+/* 
+#define RADIO_DAP_0             (*(volatile uint32_t*) (RADIO_BASE+0x620))   // Device address prefix 0
+#define RADIO_DAP_1             (*(volatile uint32_t*) (RADIO_BASE+0x624))   // 
+#define RADIO_DAP_2             (*(volatile uint32_t*) (RADIO_BASE+0x628))   // 
+#define RADIO_DAP_3             (*(volatile uint32_t*) (RADIO_BASE+0x62C))   // 
+#define RADIO_DAP_4             (*(volatile uint32_t*) (RADIO_BASE+0x630))   // 
+#define RADIO_DAP_5             (*(volatile uint32_t*) (RADIO_BASE+0x634))   // 
+#define RADIO_DAP_6             (*(volatile uint32_t*) (RADIO_BASE+0x638))   // 
+#define RADIO_DAP_7             (*(volatile uint32_t*) (RADIO_BASE+0x63C))   // 
+*/
+
+#define RADIO_DACNF             (*(volatile uint32_t*) (RADIO_BASE+0x640))   // Device address match configuration
+
+#define RADIO_OVERRIDE          ((volatile uint32_t[5]) (RADIO_BASE+0x724))  // Trim value override
+/*
+#define RADIO_OVERRIDE_0        (*(volatile uint32_t*) (RADIO_BASE+0x724))   // Trim value override register 0
+#define RADIO_OVERRIDE_1        (*(volatile uint32_t*) (RADIO_BASE+0x728))   // 
+#define RADIO_OVERRIDE_2        (*(volatile uint32_t*) (RADIO_BASE+0x72C))   // 
+#define RADIO_OVERRIDE_3        (*(volatile uint32_t*) (RADIO_BASE+0x730))   // 
+#define RADIO_OVERRIDE_4        (*(volatile uint32_t*) (RADIO_BASE+0x734))   // 
+*/
+
+#define RADIO_POWER             (*(volatile uint32_t*) (RADIO_BASE+0xFFC))   // Peripheral power control
+
+
+/*
+ * Valid register values
+ */
+
+// for RADIO_SHORTS
+#define RADIO_SHORTCUT_READY_START          0x001
+#define RADIO_SHORTCUT_END_DISABLE          0x002
+#define RADIO_SHORTCUT_DISABLED_TXEN        0x004
+#define RADIO_SHORTCUT_DISABLED_RXEN        0x008
+#define RADIO_SHORTCUT_ADDRESS_RSSISTART    0x010
+#define RADIO_SHORTCUT_END_START            0x020
+#define RADIO_SHORTCUT_ADDRESS_BCSTART      0x040
+#define RADIO_SHORTCUT_DISABLED_RSSISTOP    0x100
+
+// for RADIO_INTENSET and RADIO_INTENCLR
+#define RADIO_INTERRUPT_READY               0x001
+#define RADIO_INTERRUPT_ADDRESS             0x002
+#define RADIO_INTERRUPT_PAYLOAD             0x004
+#define RADIO_INTERRUPT_END                 0x008
+#define RADIO_INTERRUPT_DISABLED            0x010
+#define RADIO_INTERRUPT_DEVMATCH            0x020
+#define RADIO_INTERRUPT_DEVMISS             0x040
+#define RADIO_INTERRUPT_RSSIEND             0x080
+#define RADIO_INTERRUPT_BCMATCH             0x400
+
+// for RADIO_TXPOWER
+#define RADIO_TXPOWER_POS4DBM               0x04
+#define RADIO_TXPOWER_0DBM                  0x00
+#define RADIO_TXPOWER_NEG4DBM               0xFC 
+#define RADIO_TXPOWER_NEG8DBM               0xF8
+#define RADIO_TXPOWER_NEG12DBM              0xF4
+#define RADIO_TXPOWER_NEG16DBM              0xF0
+#define RADIO_TXPOWER_NEG20DBM              0xEC
+#define RADIO_TXPOWER_NEG30DBM              0xD8
+
+// for RADIO_MODE
+#define RADIO_MODE_NRF_1MBIT                0
+#define RADIO_MODE_NRF_2MBIT                1
+#define RADIO_MODE NRF_250KBIT              2
+#define RADIO_MODE_BLE_1MBIT                3
+
+// for RADIO_TXADDRESS
+#define RADIO_TXADDR0                       0
+#define RADIO_TXADDR1                       1
+#define RADIO_TXADDR2                       2
+#define RADIO_TXADDR3                       3
+#define RADIO_TXADDR4                       4
+#define RADIO_TXADDR5                       5
+#define RADIO_TXADDR6                       6
+#define RADIO_TXADDR7                       7
+
+// for RADIO_RXADDRESSES
+#define RADIO_RXADDR0                      (1 << 0)
+#define RADIO_RXADDR1                      (1 << 1)
+#define RADIO_RXADDR2                      (1 << 2)
+#define RADIO_RXADDR3                      (1 << 3)
+#define RADIO_RXADDR4                      (1 << 4)
+#define RADIO_RXADDR5                      (1 << 5)
+#define RADIO_RXADDR6                      (1 << 6)
+#define RADIO_RXADDR7                      (1 << 7)
+
+// for RADIO_CRCCNF
+#define RADIO_CRCCNF_LEN_DISABLED           0
+#define RADIO_CRCCNF_LEN_1                  1
+#define RADIO_CRCCNF_LEN_2                  2
+#define RADIO_CRCCNF_LEN_3                  3
+#define RADIO_CRCCNF_INCLADDR              (0 << 8)
+#define RADIO_CRCCNF_SKIPADDR              (1 << 8)
+
+// for RADIO_STATE
+#define RADIO_STATE_DISABLED                0
+#define RADIO_STATE_RXRU                    1
+#define RADIO_STATE_RXIDLE                  2
+#define RADIO_STATE_RX                      3
+#define RADIO_STATE_RXDISABLE               4
+#define RADIO_STATE_TXRU                    9
+#define RADIO_STATE_TXIDLE                  10
+#define RADIO_STATE_TX                      11
+#define RADIO_STATE_TXDISABLE               12
+
+
+/*
+ * Register macros
+ */
+#define radio_crcstatus_ok                 (RADIO_CRCSTATUS == 1)
+
+
+    RADIO_PCNF1 =
+
+    radio_data_whitening_enable;
+        (RADIO_PCNF1_WHITEEN_Enabled << RADIO_PCNF1_WHITEEN_Pos) |
+
+    radio_set_max_payload_length(MAX_PAYLOAD_LENGTH);
+        (MAX_PAYLOAD_LENGTH << RADIO_PCNF1_MAXLEN_Pos) |
+
+    radio_set_access_address_size();
+        (3UL << RADIO_PCNF1_BALEN_Pos);
+
+
+/*
+ * Link Layer
+ * Bluetooth Core 4.1 specification, section 2.1, page 2503
+ */
+#define RADIO_MAX_PDU			39
+#define RADIO_MIN_PDU			2
+
+#define RADIO_FLAGS_RX_NEXT		1
+#define RADIO_FLAGS_TX_NEXT		2
+
+
+/* The active parameter informs if the radio is currently active (e.g. because
+ * of a TX/RX_NEXT flag). So, if the callback implementation wants to operate
+ * the radio, it will need to first stop the radio.
+ */
+typedef void (*radio_recv_callback_t) (const uint8_t *pdu, bool crc, bool active);
+typedef void (*radio_send_callback_t) (bool active);
+
+int16_t radio_init(void);
+
+int16_t radio_set_callbacks(radio_recv_callback_t recv_callback, radio_send_callback_t send_callback);
+int16_t radio_prepare(uint8_t ch, uint32_t aa, uint32_t crcinit);
+int16_t radio_recv(uint32_t flags);
+int16_t radio_send(const uint8_t *data, uint32_t flags);
+int16_t radio_stop(void);
+
+int16_t radio_set_tx_power(radio_power_t power);
+void radio_set_out_buffer(uint8_t *buf);
+
+#endif RADIO_H
