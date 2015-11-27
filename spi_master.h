5ba8346044f1b43b27745a322c67a702e3c2a9f5
Matthias <mail@matthiasbock.net>
2015-11-27 18:05:49 +0100

    spi_master.h: minor cosmetics

--- spi_master.h
+++ spi_master.h
@@ -40,44 +40,44 @@
  * Applicable register values
  */
 // Pin selection
-#define SPI_PIN_DISABLED        0xFFFFFFFF
+#define SPI_PIN_DISABLED                0xFFFFFFFF
 
 // Data rate
-#define SPI_FREQUENCY_125K      0x02000000      // 125 kbps
-#define SPI_FREQUENCY_250K      0x04000000      // 250 kbps
-#define SPI_FREQUENCY_500K      0x08000000      // 500 kbps
-#define SPI_FREQUENCY_1M        0x10000000      // 1 Mbps
-#define SPI_FREQUENCY_2M        0x20000000      // 2 Mbps
-#define SPI_FREQUENCY_4M        0x40000000      // 4 Mbps
-#define SPI_FREQUENCY_8M        0x80000000      // 8 Mbps
+#define SPI_FREQUENCY_125K              0x02000000      // 125 kbps
+#define SPI_FREQUENCY_250K              0x04000000      // 250 kbps
+#define SPI_FREQUENCY_500K              0x08000000      // 500 kbps
+#define SPI_FREQUENCY_1M                0x10000000      // 1 Mbps
+#define SPI_FREQUENCY_2M                0x20000000      // 2 Mbps
+#define SPI_FREQUENCY_4M                0x40000000      // 4 Mbps
+#define SPI_FREQUENCY_8M                0x80000000      // 8 Mbps
 
 // Configuration
 #define SPI_BITORDER_MSBFIRST           0   // Most significant bit is shifted out first
 #define SPI_BITORDER_LSBFIRST           1   // Least significant bit is shifted out first
 
-#define SPI_CLOCKPHASE_LEADING          0   // Data is sampled on the leading edge of clock and shifted out on the trailing edge
-#define SPI_CLOCKPHASE_TRAILING         2   // Data is sampled on the trailing edge of clock and shifted out on the leading edge
+#define SPI_CLOCKPHASE_LEADING          0       // Data is sampled on the leading edge of clock and shifted out on the trailing edge
+#define SPI_CLOCKPHASE_TRAILING        (1 << 1) // Data is sampled on the trailing edge of clock and shifted out on the leading edge
 
 #define SPI_CLOCKPOLARITY_ACTIVEHIGH    0   // self explaining ...
-#define SPI_CLOCKPOLARITY_ACTIVELOW     4
+#define SPI_CLOCKPOLARITY_ACTIVELOW    (1 << 2)
 
 /*
  * Register macros
  */
 // Enable/Disable
-#define spi_enabled(spi_device)                        (SPI_ENABLE(spi_device) & 0x07)
-#define spi_enable(spi_device)                          SPI_ENABLE(spi_device) |= 1      // set bit
-#define spi_disable(spi_device)                         SPI_ENABLE(spi_device) &= ~0x07  // clear bit
+#define spi_enabled(spi_device)                        (SPI_ENABLE(spi_device) & 0x07 == 1)  // beware: a value of 5 means TWI is enabled
+#define spi_enable(spi_device)                          SPI_ENABLE(spi_device) = (SPI_ENABLE(spi_device) & (~0x07)) | 1
+#define spi_disable(spi_device)                         SPI_ENABLE(spi_device) = (SPI_ENABLE(spi_device) & (~0x07))
 
 // Interrupts
 #define spi_interrupt_upon_READY(spi_device)          ((SPI_INTEN   (spi_device)) & 0x04) >> 2)
-#define spi_interrupt_upon_READY_enable(spi_device)     SPI_INTENSET(spi_device) = 0x04
-#define spi_interrupt_upon_READY_disable(spi_device)    SPI_INTENCLR(spi_device) = 0x04
+#define spi_interrupt_upon_READY_enable(spi_device)     SPI_INTENSET(spi_device)  = 0x04
+#define spi_interrupt_upon_READY_disable(spi_device)    SPI_INTENCLR(spi_device)  = 0x04
 
 // Pins to use: [0..31]
 #define spi_pin_select(spi_device, pin_SCK, pin_MOSI, pin_MISO) \
 { \
-    SPI_PSELSCK(spi_device)  = pin_SCK;     \
+    SPI_PSELSCK (spi_device) = pin_SCK;     \
     SPI_PSELMOSI(spi_device) = pin_MOSI;    \
     SPI_PSELMISO(spi_device) = pin_MISO;    \
 };
