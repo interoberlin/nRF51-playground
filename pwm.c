e85a6e2ad717278039b5afe86027b2b72621fd7a
Matthias <mail@matthiasbock.net>
2016-01-15 23:20:04 +0100

    PWM library: first implementation complete

--- pwm.c
+++ pwm.c
@@ -3,7 +3,7 @@
  * Pulse Width Modulation (PWM) library
  * for the Nordic Semiconductor nRF51 series
  *
- * Authors: Matthias Bock <mail@matthiasbock.net>
+ * Author: Matthias Bock <mail@matthiasbock.net>
  * License: GNU GPLv3
  *
  * Read more about the implementation approach:
@@ -16,58 +16,73 @@
 void pwm_setup(
         pwm_t*   pwm,
         uint8_t  gpio_pin,
-        uint8_t  gpiote_channel,
-        uint8_t  ppi_channel,
+        uint8_t  gpiote_channel0,
+        uint8_t  gpiote_channel1,
         uint32_t timer,
         uint8_t  timer_counter0,
         uint8_t  timer_counter1,
-        uint8_t  timer_bitmode
+        uint8_t  timer_bitmode,
+        uint8_t  ppi_channel0,
+        uint8_t  ppi_channel1
         )
 {
+    // fill PWM struct with arguments
+    pwm->gpio_pin           = gpio_pin;
+    pwm->gpiote_channel0    = gpiote_channel0;
+    pwm->gpiote_channel1    = gpiote_channel1;
+    pwm->timer              = timer;
+    pwm->timer_counter0     = timer_counter0;
+    pwm->timer_counter1     = timer_counter1;
+    pwm->ppi_channel0       = ppi_channel0;
+    pwm->ppi_channel1       = ppi_channel1;
+
     // configure GPIO tasks and events
-    GPIOTE_CONFIG[0] = GPIOTE_MODE_TASK | GPIOTE_PSEL(gpio_pin) | GPIOTE_POLARITY_HighToLow;
-    GPIOTE_CONFIG[1] = GPIOTE_MODE_TASK | GPIOTE_PSEL(gpio_pin) | GPIOTE_POLARITY_LowToHigh;
+    GPIOTE_CONFIG[pwm->gpiote_channel0] = GPIOTE_MODE_TASK | GPIOTE_PSEL(pwm->gpio_pin) | GPIOTE_POLARITY_HIGH_TO_LOW;
+    GPIOTE_CONFIG[pwm->gpiote_channel1] = GPIOTE_MODE_TASK | GPIOTE_PSEL(pwm->gpio_pin) | GPIOTE_POLARITY_LOW_TO_HIGH;
 
     // configure timer: frequency, mode, shortcuts and counter values
-    TIMER0_PRESCALER = 23; // 128 Hz
-    TIMER0_MODE = TIMER_MODE_COUNTUP;
-    TIMER0_BITMODE = TIMER_BITMODE_8BIT;
+    TIMER_PRESCALER(pwm->timer)  = 23; // 128 Hz
+    TIMER_MODE(pwm->timer)       = TIMER_MODE_COUNTUP;
+    TIMER_BITMODE(pwm->timer)    = TIMER_BITMODE_8BIT;
 
     // trigger an event, when the counter is set to zero
-    TIMER0_CC[0] = 0;
+    TIMER_CC(pwm->timer)[pwm->timer_counter0] = 0;
     // trigger an event, when the counter reaches 128 (50%)
-    TIMER0_CC[1] = 128;
+    TIMER_CC(pwm->timer)[pwm->timer_counter1] = 128;
 
-    // configure PPI connection between GPIOTE and Timer
-    PPI_CHANNEL[0]->
-    EVENT = &TIMER0_EVENT_COMPARE[0];
-    TASK  = &GPIOTE_OUT[0];
+    // configure PPI connection between GPIOTE and Timer:
 
-    PPI_CHANNEL[1]->
-    EVENT = &TIMER0_EVENT_COMPARE[1];
-    TASK  = &GPIOTE_OUT[1];
+    // connect "Timer counter reaches zero"-event to "Switch GPIO pin to HIGH"-task
+    PPI_CH[pwm->ppi_channel0]->EEP = &TIMER_EVENT_COMPARE(pwm->timer)[pwm->timer_counter0];
+    PPI_CH[pwm->ppi_channel0]->TEP = &GPIOTE_TASK_OUT[pwm->gpiote_channel0];
+
+    // connect "Timer counter reaches counter1 value"-event to "Switch GPIO pin to LOW"-task
+    PPI_CH[pwm->ppi_channel1]->EEP = &TIMER_EVENT_COMPARE(pwm->timer)[pwm->timer_counter1];
+    PPI_CH[pwm->ppi_channel1]->TEP = &GPIOTE_TASK_OUT[pwm->gpiote_channel1];
 }
 
 void pwm_start(pwm_t* pwm)
 {
-    // enable the configured PPI channel
-    PPI_CHENSET(pwm->ppi_channel) = 1;
+    // enable the configured PPI channels
+    PPI_CHENSET(pwm->ppi_channel0) = 1;
+    PPI_CHENSET(pwm->ppi_channel1) = 1;
 
     // enable Timer
-    TIMER0_START = 1;
+    TIMER_START(pwm->timer) = 1;
 }
 
 void pwm_write(pwm_t* pwm, uint32_t value)
 {
     // adjust switch-low counter value
-    TIMER0_CC[1] = value;
+    TIMER_CC(pwm->timer)[pwm->timer_counter1] = value;
 }
 
 void pwm_stop(pwm_t* pwm)
 {
-    // disable the configured PPI channel
-    PPI_CHENCLR(pwm->ppi_channel) = 1;
+    // disable the configured PPI channels
+    PPI_CHENCLR(pwm->ppi_channel0) = 1;
+    PPI_CHENCLR(pwm->ppi_channel1) = 1;
 
     // stop/shutdown Timer
-    TIMER0_SHUTDOWN = 1;
+    TIMER_SHUTDOWN(pwm->timer) = 1;
 }
