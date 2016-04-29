05071ef51ad9f17ca10f1dedd2a16a2e50b98e59
Matthias <mail@matthiasbock.net>
2016-04-29 22:19:46 +0200

    new function allows to add 5 bits to FIFO per write

--- fifo.c
+++ fifo.c
@@ -16,12 +16,13 @@ bool fifo_read(struct fifo_s *fifo, char *dst)
     if (!fifo_available(fifo))
         return false;
 
-    // circular buffer
-    fifo->index_read = (fifo->index_read + 1) % FIFO_SIZE;
-    // decrement number of available bytes
-    fifo->num_available--;
     // read char from FIFO
     *dst = fifo->buffer[fifo->index_read];
+    // decrement number of available bytes
+    fifo->num_available--;
+
+    // circular buffer
+    fifo->index_read = (fifo->index_read + 1) % FIFO_SIZE;
 
     // success
     return true;
@@ -35,12 +36,57 @@ bool fifo_write(struct fifo_s *fifo, char *c)
     if (fifo_full(fifo))
         return false;
 
-    // circular buffer
-    fifo->index_write = (fifo->index_write + 1) % FIFO_SIZE;
-    // increment number of available bytes
-    fifo->num_available++;
     // write one byte to FIFO
     fifo->buffer[fifo->index_write] = *c;
+    // increment number of available bytes
+    fifo->num_available++;
+
+    // circular buffer
+    fifo->index_write = (fifo->index_write + 1) % FIFO_SIZE;
+
+    // success
+    return true;
+}
+
+/*
+ * Write 5 bits to FIFO
+ */
+bool fifo_write_5bits(fifo_t* fifo, uint8_t* value_5bits)
+{
+    if (fifo_full(fifo))
+        return false;
+
+    // the FIFO byte to alter at first
+    uint8_t index_current_byte             = fifo->index_write_bit % 8;
+
+    // how much, do we need to shift the input to fit the bit frame
+    uint8_t bits_shift_left                = fifo->index_write_bit - (index_current_byte*8);
+
+    uint8_t value                          = (*value_5bits) & 0x1f;
+    uint8_t apply_mask                     = value << bits_shift_left;
+
+    // clear higher bits of current byte
+    fifo->buffer[index_current_byte]      &= 0xff >> (8-bits_shift_left);
+
+    // apply bitmask
+    fifo->buffer[index_current_byte]      |= apply_mask;
+
+
+    // the next FIFO byte, that (potentially) must be changed too
+    if (bits_shift_left+5 >= 8)
+    {
+        // increment number of available bytes
+        fifo->num_available++;
+
+        uint8_t index_next_byte             = (index_current_byte+1) % FIFO_SIZE;
+
+        uint8_t bits_shift_right            = (bits_shift_left+5) % 8;
+
+        fifo->buffer[index_next_byte]       = value >> bits_shift_right;
+    }
+
+    // circular buffer
+    fifo->index_write_bit = (fifo->index_write_bit + 5) % FIFO_SIZE_BITS;
 
     // success
     return true;
--- fifo.h
+++ fifo.h
@@ -15,7 +15,8 @@
 
 #include "delay.h"
 
-#define FIFO_SIZE 1024
+#define FIFO_SIZE       1024
+#define FIFO_SIZE_BITS  FIFO_SIZE*8
 
 /*
  * A memory area of FIFO_SIZE bytes
@@ -25,21 +26,27 @@ struct fifo_s
 {
     volatile uint32_t index_read;
     volatile uint32_t index_write;
+    volatile uint32_t index_write_bit;
     volatile uint8_t  buffer[FIFO_SIZE];
     volatile uint32_t num_available;
 };
+typedef struct fifo_s fifo_t;
 
 #define fifo_init(fifo) \
 { \
-    fifo->index_read     = 0; \
-    fifo->index_write    = 0; \
-    fifo->num_available  = 0; \
+    fifo->index_read      = 0; \
+    fifo->index_write     = 0; \
+    fifo->index_write_bit = 0; \
+    fifo->num_available   = 0; \
 }
 
 #define fifo_available(fifo)    ( fifo->num_available > 0 )
 #define fifo_full(fifo)         ( fifo->num_available >= FIFO_SIZE )
 
-bool fifo_read(struct fifo_s *fifo, char *dst);
-bool fifo_write(struct fifo_s *fifo, char *c);
+bool fifo_read(fifo_t* fifo, char *dst);
+bool fifo_write(fifo_t* fifo, char *c);
+
+// a special function for WS2811 SPI control: write 5 bits to FIFO
+bool fifo_write_5bits(fifo_t* fifo, uint8_t* value_5bits);
 
 #endif
