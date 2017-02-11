89a8cccf43c3d7c777b5818e6db98b58a973dc48
Matthias <mail@matthiasbock.net>
2017-02-11 22:17:36 +0100

    fixed UART demo to work with latest SDK commit

--- demo_uart.c
+++ demo_uart.c
@@ -18,31 +18,31 @@
 
 void uart_setup()
 {
-    uart_disable;
+    uart_disable();
 
     // configure transmission
     nrf_gpio_pin_dir_set(PIN_UART_TXD, NRF_GPIO_PIN_DIR_OUTPUT);
     uart_select_pin_as_TXD(PIN_UART_TXD);
     uart_select_pin_as_RTS(UART_PIN_DISABLE);
     uart_select_pin_as_CTS(UART_PIN_DISABLE);
-    uart_set_baud(baud_9600);
-    uart_set_parity_exclude;
-    uart_flow_control_enable;
+    uart_set_baud(UART_BAUD_9600);
+    uart_set_parity_exclude();
+    uart_flow_control_enable();
 
     // configure reception
     uart_select_pin_as_RXD(PIN_UART_RXD);
-    uart_interrupt_upon_RXDRDY_enable;
-    uart_interrupt_upon_RXTO_enable;
-    uart_interrupt_upon_ERROR_enable;
-    uart_start_receiver;
+    uart_interrupt_upon_RXDRDY_enable();
+    uart_interrupt_upon_RXTO_enable();
+    uart_interrupt_upon_ERROR_enable();
+    uart_start_receiver();
 
-    uart_enable;
+    uart_enable();
 }
 
 int main()
 {
     uart_setup();
-    uart_send("\xD\xAnRF51822:~$ ", 14);
+    uart_send_bytes("\xD\xAnRF51822:~$ ", 14);
     
     while (true)
     {
